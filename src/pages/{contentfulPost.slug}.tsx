376dc9e9aa01d4326ff56c7832c2d2fb25857c74
juhun <minjuhun1@gmail.com>
2025-02-04 15:36:20 +0900

    02.04 게시글 정보 컴포넌트 구현하기기

--- src/pages/{contentfulPost.slug}.tsx
+++ src/pages/{contentfulPost.slug}.tsx
@@ -1,5 +1,33 @@
-import { PageProps } from 'gatsby'
+import { PageProps, graphql } from 'gatsby'
+import { IGatsbyImageData } from 'gatsby-plugin-image'
+import PostHead from '../components/post/PostHead'
 
-export default function Post({ params }: PageProps) {
-  return <div>{params.slug}</div>
+export default function Post({
+  data: { contentfulPost },
+}: PageProps<Queries.PostPageQuery>) {
+  return (
+    <>
+      <PostHead
+        title={contentfulPost?.title as string}
+        category={contentfulPost?.category as string[]}
+        date={contentfulPost?.date as string}
+        thumbnail={
+          contentfulPost?.thumbnail?.gatsbyImageData as IGatsbyImageData
+        }
+      />
+    </>
+  )
 }
+
+export const query = graphql`
+  query PostPage($slug: String!) {
+    contentfulPost(slug: { eq: $slug }) {
+      title
+      thumbnail {
+        gatsbyImageData(width: 1000)
+      }
+      category
+      date
+    }
+  }
+`
